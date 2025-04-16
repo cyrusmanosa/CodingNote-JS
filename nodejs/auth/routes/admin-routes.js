3b13ba467ec890f550a01dcf188e101de76f0481
Cyrus Man <114724876+cyrusmanosa@users.noreply.github.com>
2025-04-16 16:28:18 +0800

    Normalize .gitignore line endings

--- nodejs/auth/routes/admin-routes.js
+++ nodejs/auth/routes/admin-routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const authMiddleware = require('../middleware/auth-middleware');
+const adminMiddleware = require('../middleware/admin-middleware');
 const router = express.Router();
 
-router.get('/welcome', (req, res) => {
+router.get('/welcome',authMiddleware,adminMiddleware, (req, res) => {
     res.json({ message: 'Welcome to the admin page!' });
 });
--- nodejs/auth/server.js
+++ nodejs/auth/server.js
@@ -5,6 +5,7 @@ const connectDB = require('./database/db');
 const PORT = process.env.PORT || 3000;
 const authRoutes = require('./routes/auth-routes');
 const homeRoutes = require('./routes/home-routes');
+const adminRoutes = require('./routes/admin-routes');
 
 connectDB();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes); 
 app.use('/api/home', homeRoutes); 
+app.use('/api/admin', adminRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
